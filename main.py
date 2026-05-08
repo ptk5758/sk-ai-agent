43a7564410cd918ab9baa8b23ebe7dfcd73f104e
ptk5758 <ptk725739@gmail.com>
2026-05-09 01:04:59 +0900

    [chore] CORS 설정

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from src.api.v1.ingredient import router as ingredient_router
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # 허용할 도메인 목록
+    allow_credentials=True,           # 쿠키 포함 여부 설정
+    allow_methods=["*"],              # 모든 HTTP Method 허용 (GET, POST 등)
+    allow_headers=["*"],              # 모든 HTTP Header 허용
+)
 
 app.include_router(ingredient_router)
